41fcab9d731b99b34d5c606185639f7d5f4503dc
Prismika Gurung <prismikagurung1@gmail.com>
2025-02-09 23:35:06 +0000

    Added MongoDB integration

--- server.js
+++ server.js
@@ -1,9 +1,19 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
 const app = express();
 const PORT = 3000;
+const MONGO_URL = process.env.MONGO_URL || "mongodb://localhost:27017/mydatabase";
+
+mongoose.connect(MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+.then(() => console.log("Connected to MongoDB"))
+.catch(err => console.error("MongoDB connection error:", err));
 
 app.get('/', (req, res) => {
-    res.send('Hello, Docker!');
+    res.send("Hello, Docker with MongoDB!");
 });
 
 app.listen(PORT, () => {
